6ebfcef3f3d6c949815821a905c5977290a7e6b4
Ivan <monin535@yandex.ru>
2024-09-30 17:32:26 +0300

    Added find func

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -45,13 +45,22 @@ void printTree(Tree* root,int size) {
 
     printTree(root->left, size + 1);
 }
-
+int find(Tree* root, int data,int count) {
+    if (root == NULL) {
+        return count;
+    }
+    if (root->data == data)count++;
+    count =find(root->right, data,count);
+    count =find(root->left, data, count);
+    return count;
+}
 void displayMenu(Tree* root) {
-    int data = 0;;
+    int data = 0;
+    int k = 0;
     printf("----------Menu----------\n");
     printf("1. Add element\n");
     printf("2. Display elements\n");
-    printf("3. Delete name\n");
+    printf("3. Find elements\n");
     int choice;
     scanf("%d", &choice);
     system("cls");
@@ -81,13 +90,17 @@ void displayMenu(Tree* root) {
         system("cls");
         displayMenu(root);
         break;
-   /* case(3):
-        deleteElem(q);
-        printf("\nThe element deleted.\nTap some button to return to main menu...");
+    case(3):
+        printf("What element do u want to find? ");
+        scanf("%d", &data);
+        k = find(root, data, 0);
+        printf("There are only %d elements in the tree..\n", k);
+        printf("Tap some button to return to main menu...");
+        getchar();
         getchar();
         system("cls");
-        displayMenu(q);
-        break;*/
+        displayMenu(root);
+        break;
     default:
         printf("Wrong button!");
         displayMenu(root);
