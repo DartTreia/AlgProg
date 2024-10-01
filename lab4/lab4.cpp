012f22e2e5ac8fa8f7badc0bfb304c786a821952
Ivan <monin535@yandex.ru>
2024-10-01 12:39:45 +0300

    Practice tasks

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -3,13 +3,49 @@
 #include <stdlib.h>
 #include <math.h>
 
+int max = 0;
+int k = 0;//кол-во повторов
+
 typedef struct Tree {
     int data;
     struct Tree* left;
     struct Tree* right;
 } Tree;
 
+void findMaxLvl(Tree* root,int count) {
+    if (root == NULL) {
+        return ;
+    }
+    if (root->right != NULL || root->left != NULL) {
+        count++;
+    }
+    else {
+        if (count > max)max = count;
+        count = 0;
+    }
+    findMaxLvl(root->right, count);
+    findMaxLvl(root->left, count);
+}
 
+void findMaxRepeatElemLvl(Tree* root, int data,int count) {
+    if (root == NULL) {
+        return;
+    }
+    if (k>0 && root->data == data) {
+        if (count > max)max = count;
+        k--;
+        if (k == 0) {
+            printf("The max lvl of this repeated element is %d\n", max);
+        }
+    }
+    if (root->right != NULL || root->left != NULL) {
+        count++;
+    }
+    else { count = 0; }
+   
+    findMaxRepeatElemLvl(root->right, data, count);
+    findMaxRepeatElemLvl(root->left, data, count);
+}
 
 Tree* addElem(Tree* root, int data) {
     Tree* tmp = NULL;
@@ -100,6 +136,8 @@ void displayMenu(Tree* root) {
     printf("3. Display elements\n");
     printf("4. Find elements\n");
     printf("5. Calculate difficult of find function by value\n");
+    printf("6. Max lvl of the tree\n");
+    printf("7. Max lvl of the repeated element\n");
     int choice;
     scanf("%d", &choice);
     system("cls");
@@ -166,6 +204,29 @@ void displayMenu(Tree* root) {
         getchar();
         getchar();
         system("cls");
+        displayMenu(root);
+        break;
+    case(6):
+        findMaxLvl(root, 0);
+        printf("The max lvl of the tree is %d\n", max);
+        printf("Tap some button to return to main menu...");
+        getchar();
+        getchar();
+        system("cls");
+        max = 0;
+        displayMenu(root);
+        break;
+    case(7):
+        printf("What element do u want to find? ");
+        scanf("%d", &data);
+        k = find(root, data, 0);
+        findMaxRepeatElemLvl(root, data,0);
+        printf("Tap some button to return to main menu...");
+        getchar();
+        getchar();
+        system("cls");
+        max = 0;
+        k = 0;
         displayMenu(root);
         break;
     default:
