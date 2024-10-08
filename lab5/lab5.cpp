c783cf1396163428a22ed379187be4cbbf36c20b
Ivan <monin535@yandex.ru>
2024-10-08 12:49:04 +0300

    Fixed code 2

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <string.h>
 void main() {
 	srand(time(NULL));
+	printf("Matrix of adjancency\n\n");
 	int matrix[5][5];
+	int matrixInc[5][10];
 	int matrixSize = 0;
 	int degree = 0;
 	for (int i = 0; i < 5; i++) {
@@ -39,28 +41,40 @@ void main() {
 		if(5-degree==1)printf("%d is a dominated vertex\n", i+1);
 		degree = 0;
 	}
-	matrixSize = 0;
-	////////
-	int maxOne = 2;
+	int col=0, row = 0;
 	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			matrix[j][i] = rand() % 2;
-			if (matrix[j][i] == 1) {
-				if (maxOne > 0) maxOne--;
-				else matrix[j][i] = 0;
-			}
-			if (j == 4 && maxOne == 1) matrix[j][i] = 1;
-			if (matrix[j][i] == 1) {
-				matrixSize++;
+		for (int j =i; j < 5; j++)
+		{
+			if (matrix[i][j] == 1) {
+				matrixInc[row+i][col] = 1;
+				matrixInc[row+j][col] = 1;
+				col++;
+				if (col == matrixSize) {
+					i = 5;
+					j = 5;
+				}
 			}
 		}
-		maxOne = 2;
 	}
-	printf("\n\n   a b c d e\n\n");
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < matrixSize; j++)
+		{
+			if (matrixInc[i][j] != 1) {
+				matrixInc[i][j] = 0;
+			}
+		}
+	}
+	printf("\n   ");
+	char string[20] = "A B C D E F G H I J";
+	for (int i = 0; i < 2 * matrixSize; i++) {
+		printf("%c", string[i]);
+	}
+	printf("\n\n");
 	for (int i = 0; i < 5; i++) {
 		printf("%d  ", i + 1);
-		for (int j = 0; j < 5; j++) {
-			printf("%d ", matrix[i][j]);
+		for (int j = 0; j < matrixSize; j++)
+		{
+			printf("%d ", matrixInc[i][j]);
 		}
 		printf("\n");
 	}
